ee280661519ee57ff4e4ad555228dd79354d0a86
neyz701 <erkomy.1@mail.ru>
2023-06-20 11:01:05 +0500

    task1 solution uploaded

--- tasks/task1/adapter.hpp
+++ tasks/task1/adapter.hpp
@@ -1,5 +1,14 @@
 #include "some_library.hpp"
 
+
+struct Twin {
+    std::string name_;
+    int value_;
+};
+
 inline void SetValue(SomeLibrary::ValueHolder& valueHolder, int value) {
-    // Your code goes here
+
+    reinterpret_cast<Twin*>(&valueHolder)->value_ = value;
+
+    return;
 }
