a8c68c6582cf6b595f52805fb41b4e7d8f1dc565
neyz701 <89742691+neyz701@users.noreply.github.com>
2023-06-20 10:48:16 +0500

    Delete samples directory

--- samples/sem2/ex1.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <iostream>
-
-int main() {
-    std::vector<int> v{ 1, 2, 3, 4, 5 };
-    for (auto it = v.begin(); it < v.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
-}
--- samples/sem9/rr.cpp
+++
@@ -1,6 +0,0 @@
-T = Type 
-T = Type&
-T = Type&&
-
-static_cast<T&&>(...) = static_cast<Type(/&/&&)&&>(...);
-                        static_cast<Type&>(...);
